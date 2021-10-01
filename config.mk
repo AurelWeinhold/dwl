ed2e2ac8012f6e342e1198b792d298ac6d1b8dd8
Aurel Weinhold <aurel@theta.localdomain>
2021-10-01 22:17:59 +0200

    Disable xWayland

--- config.mk
+++ config.mk
@@ -8,8 +8,9 @@ MANDIR = $(PREFIX)/share/man
 # Compile flags that can be used
 #CFLAGS = -pedantic -Wall -Wextra -Werror -Wno-unused-parameter -Wno-sign-compare -Wno-unused-function -Wno-unused-variable -Wno-unused-result -Wdeclaration-after-statement
 
-XWAYLAND =
-XLIBS =
 # Uncomment to build XWayland support
 #XWAYLAND = -DXWAYLAND
 #XLIBS = xcb
+XWAYLAND =
+XLIBS =
+#CFLAGS += -DXWAYLAND
